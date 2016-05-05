f32ae3f401113ecb566eaf7431ad156f5da3cac8
catnewsham <catherine.newsham@gmail.com>
2016-05-05 13:13:06 +0200

    Data and script for Titanic exercise - data camp

---
+++ Data Camp - Titanic script.R
@@ -0,0 +1,22 @@
+install.packages('ggplot2')
+library('ggplot2')
+titanic <- read.csv("titanic_original.csv", header=TRUE)
+# Check out the structure of titanic
+str(titanic)
+
+# Use ggplot() for the first instruction
+ggplot(titanic, aes(x = factor(Pclass), fill = factor(Sex))) + 
+  geom_bar(position = "dodge")
+
+# Use ggplot() for the second instruction
+ggplot(titanic, aes(x = factor(pclass), fill = factor(sex))) + 
+  geom_bar(position = "dodge") + 
+  facet_grid(. ~ survived)
+
+# Position jitter (use below)
+posn.j <- position_jitter(0.5, 0)
+
+# Use ggplot() for the last instruction
+ggplot(titanic, aes(x = factor(pclass), y = age, col = factor(sex))) + 
+  geom_jitter(size = 3, alpha = 0.5, position = posn.j) + 
+  facet_grid(. ~ survived)
